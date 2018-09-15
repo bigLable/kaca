a985f6a991cc0e3e2e94cb63ebd10cd8a08b1758
yuki <momojuncn@yahoo.co.jp>
2018-09-15 10:16:35 +0800

    第一次建立接口

--- model/wxInfo.js
+++ model/wxInfo.js
@@ -1,1 +1,22 @@
 const DAO =require('./DAO')
+class WX{
+    wximg(wxImg){
+        return DAO('select * from wx where wxImg=?',[wxImg])
+    }
+    wxname(wxName){
+        return DAO('select * from wx where wxName=?',[wxName])
+    }
+    wxfrom(wxFrom){
+        return DAO('select * from wx where wxFrom=?',[wxFrom])
+    }
+    wxtime(wxTime){
+        return DAO('select * from wx where wxTime=?',[wxTime])
+    }
+    wxPhone(wxphone){
+        return DAO('select * from wx where wxphone=?',[wxphone])
+    }
+    wxpage(wxPage){
+        return DAO('select * from wx where wxPage=?',[wxPage])
+    }
+
+}

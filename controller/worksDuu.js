2f3bdca052f8c4155a2c3606ec05f402ce3d5957
asus <1509592281@qq.com>
2018-09-18 16:06:00 +0800

    第四次修改

--- controller/worksDuu.js
+++ controller/worksDuu.js
@@ -1,41 +1,52 @@
-const DB=require('../model/worksDAO');
-const form=require('formidable');
-module.exports= {
+const DB = require('../model/worksDAO');
+const form = require('formidable');
+module.exports = {
     getworks: async (ctx) => {
         ctx.set('Access-Control-Allow-Origin', '*');
         let jsondata = await DB.getworks()
         ctx.set('content-type', 'application/json');
         ctx.body = jsondata;
     },
-    getOneWorks:async (ctx) => {
+    getOneWorks: async (ctx) => {
         ctx.set('Access-Control-Allow-Origin', '*');
         let jsondata = await DB.getOneWorks(ctx.query.id)
         ctx.set('content-type', 'application/json');
         ctx.body = jsondata;
     },
-    addWorks:async(ctx)=>{
-        ctx.set('Access-Control-Allow-Origin','*');
-        let form= new form.IncomingForm();
-        await  form.parse(ctx,(err,fileds)=>{
-            let Revalue={
-                worksId:fileds.value1,
-                worksauthor:fileds.value2,
-                worksDescribe:fileds.value3,
-                worksEquipment:fileds.value4,
-                worksPageview:fileds.value5,
-                worksPoint:fileds.value6,
-                worksDate:fileds.value7
-            }
-            DB. addWorks(Revalue,(data)=>{
-                ctx.body={code:200,message:'add ok',data:{}}
-            });
-
-        })
+    addWorks: async (ctx, next) => {
+        console.log(ctx.request.body)
+        let works = {};
+        works.worksId = ctx.request.body.worksId;
+        works.worksPic = ctx.request.body.worksPic;
+        works.worksauthor = ctx.request.body.worksauthor;
+        works.worksDescribe = ctx.request.body.worksDescribe;
+        works.worksEquipment = ctx.request.body.worksEquipment;
+        works.worksPageview = ctx.request.body.worksPageview;
+        works.worksPoint = ctx.request.body.worksPoint;
+        works.worksDate = ctx.request.body.worksDate;
+        let jsondata =  await DB.addWorks(works);
+        ctx.body = {code: 200, message: 'message', data: jsondata}
     },
-    deleteworks:async(ctx)=>{
-        ctx.set('Access-Control-Allow-Origin','*');
-        let jsondata=await DB.deleteworks(ctx.query.id);
-        ctx.set('content-type','application/json');
-        ctx.body={code:200,message:'delete ok',data:jsondata}
+    updateworks: async (ctx, next) => {
+        console.log(ctx.request.body)
+        let work = {};
+        work.worksPic = ctx.request.body.worksPic;
+        work.worksauthor = ctx.request.body.worksauthor;
+        work.worksDescribe = ctx.request.body.worksDescribe;
+        work.worksEquipment = ctx.request.body.worksEquipment;
+        work.worksPageview = ctx.request.body.worksPageview;
+        work.worksPoint = ctx.request.body.worksPoint;
+        work.worksDate = ctx.request.body.worksDate;
+        work.worksId = ctx.request.body.worksId;
+        let jsondata = DB.updateworks(work);
+        ctx.body = {code: 200, message: 'message', data: jsondata}
+
+
+    },
+    deleteworks: async (ctx) => {
+        ctx.set('Access-Control-Allow-Origin', '*');
+        let jsondata = await DB.deleteworks(ctx.query.id);
+        ctx.set('content-type', 'application/json');
+        ctx.body = {code: 200, message: 'delete ok', data: jsondata}
     }
 }

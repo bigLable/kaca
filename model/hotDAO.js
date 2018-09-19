8c56cfe6c1228e7f7f708a600bba022f10fee59e
asus <1509592281@qq.com>
2018-09-19 10:39:31 +0800

    10.39

--- model/hotDAO.js
+++ model/hotDAO.js
@@ -2,7 +2,7 @@ const DAO=require('../model/DAO');
 
 class DB {
     gethot() {
-        //获取全部作品信息的方法
+        //获取热门商品信息的方法
         return DAO('select shopName,shopImg,shopPrice from shop where shopPara=(select max(shopPara) from shop)', [])
     }
 }

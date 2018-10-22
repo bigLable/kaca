e147813892aec398b8f523f7a5630110a0d5a912
bigYang <1216102533@qq.com>
2018-10-23 01:39:01 +0800

    9.25 18:21

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const cgqInto=require('./routes/cgqInto')
 const shopInto=require('./routes/shopInto')
 const shopcInto = require('./routes/shopcinto')
 const shoppingCart = require('./routes/shoppingCart')
+const session=require('koa-session')
 // error handler
 onerror(app)
 // app.use(async (ctx,next)=>{
@@ -23,6 +24,17 @@ onerror(app)
 //     next();
 // })
 // middlewares
+app.keys = ['some secret hurr'];
+const CONFIG = {
+    key: 'koa:sess',   //cookie key (default is koa:sess)
+    maxAge: 86400000,  // cookie的过期时间 maxAge in ms (default is 1 days)
+    overwrite: true,  //是否可以overwrite    (默认default true)
+    httpOnly: true, //cookie是否只有服务器端可以访问 httpOnly or not (default true)
+    signed: true,   //签名默认true
+    rolling: false,  //在每次请求时强行设置cookie，这将重置cookie过期时间（默认：false）
+    renew: false,  //(boolean) renew session when session is nearly expired,
+};
+app.use(session(CONFIG, app));
 app.use(bodyparser({
     enableTypes:['json', 'form', 'text']
 }))
--- controller/carDuu.js
+++ controller/carDuu.js
@@ -1,44 +1,26 @@
 const DB=require('../model/carDAO');
 const form=require('formidable');
+const session=require('koa-session')
 module.exports={
-
-    addcar:async(ctx)=>{
-        ctx.set('Access-Control-Allow-Origin','*');
-        ctx.set('content-type','application/json')
-        ctx.session.shop=[];
-
-       for(let i=0;i<ctx.session.shop.length;i++){
-           ctx.session.shop[i]=[{shopID:ctx.query.shopID,countNum:ctx.query.countNum}]
-       }
-       ctx.body={code:200,message:'add ok',data:{}}
-   },
+   //  addcar:async(ctx)=>{
+   //      ctx.set('Access-Control-Allow-Origin','*');
+   //      ctx.set('content-type','application/json')
+   //      ctx.session.shop=[];
+   //
+   //     for(let i=0;i<ctx.session.shop.length;i++){
+   //         ctx.session.shop[i]=[{shopID:ctx.query.shopID,countNum:ctx.query.countNum}]
+   //     }
+   //     ctx.body={code:200,message:'add ok',data:{}}
+   // },
     getAllcar:async(ctx)=>{
         ctx.set('Access-Control-Allow-Origin','*');
         console.log('start')
-        // for(let i=0;i<ctx.session.shop.length;i++){
-            // let  jsondata=await DB.getcarshop(ctx.session.shop[i].shopID);
-        ctx.set('content-type','application/json');
-        let  jsondata=await DB.getcarshop(10001);
-            console.log(jsondata)
+        ctx.set('content-type','application/json')
 
-            // ctx.body={code:200,message:'ok',data:jsondata,countNum:ctx.session.shop.countNum}
+        let  jsondata=await DB.getcarshop(ctx.query.ShopID);
+
         ctx.body={code:200,message:'ok',data:jsondata}
-        // }
-        // ctx.set('Access-Control-Allow-Origin','*');
-        //         // console.log('start')
-        //         // // for(let i=0;i<ctx.session.shop.length;i++){
-        //         // ctx.session.shopid=[]
-        //         // let newShop=[]
-        //         // ctx.session.shopid.push(ctx.query.ShopID)
-        //         // for(let i=0;i<ctx.session.shopid.length;i++){
-        //         //     let  jsondata=await DB.getcarshop(ctx.session.ShopID);
-        //         //     console.log(jsondata)
-        //         //     newShop.push(jsondata)
-        //         // }
-        //         // ctx.set('content-type','application/json');
-        //         // ctx.body={code:200,message:'ok',data:newShop}
-        //         //
-        //         // // }
+
 
     },
     deletecar:async(ctx)=>{

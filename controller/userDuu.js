a7a51a5679d05e5095a56487432b94943456763b
zyx <1115811239@qq.com>
2018-10-28 19:56:52 +0800

    修改路由

--- controller/userDuu.js
+++ controller/userDuu.js
@@ -2,6 +2,8 @@ var DB = require('../model/userDAO');
 var formidable = require('formidable');
 var fs =require('fs');
 var path=require('path');
+var crypto=require('crypto');
+
 module.exports = {
     getUsers: async (ctx) => {
         ctx.set('Access-Control-Allow-Origin', '*');
@@ -13,10 +15,16 @@ module.exports = {
         ctx.set('Access-Control-Allow-Origin', '*');
         ctx.set('content-type','application/json');
         console.log(ctx.request.body)
+        //密码加密
+        var pwd =ctx.request.body.userPwd;
+        const hash=crypto.createHash('md5');
+        hash.update(pwd);
+         pwd=hash.digest('hex');
         let user ={};
         user.userEmail = ctx.request.body.userEmail;
+        user.userPwd = pwd;
+        console.log(pwd)
         console.log(user.userEmail);
-        // user.userPwd = ctx.request.body.userPwd;
         let jsondata =  await DB.getOneUser(user.userEmail);
         console.log(jsondata)
         ctx.body ={"code":200,data:jsondata}
@@ -39,26 +47,27 @@ module.exports = {
         console.log('---------------')
         ctx.set('Access-Control-Allow-Origin','*');
         ctx.set('content-type','application/json')
-        var form=new formidable.IncomingForm()
-        form.multiples=false;
-        form.uploadDir='../public/headpic';
-        form.multiples = false
+        /* var form=new formidable.IncomingForm()
+         form.multiples=false;
+         form.uploadDir='../public/headpic';
+         form.multiples = false
 
-        form.parse(ctx.req,async function (err,fileds,files) {
-            console.log('------------------111',ctx.req)
-            console.log('-----------------图片-------'+files.headpic.name)
-            console.log('测试....' + files.headpic.length)
+         form.parse(ctx.req,async function (err,fileds,files) {
+             console.log('------------------111',ctx.req)
+             console.log('-----------------图片-------'+files.headpic.name)
+             console.log('测试....' + files.headpic.length)
 
-                var filename=files.headpic.name;
-                var src=files.headpic.path;
-                var press = path.join(__dirname,files.headpic.path)
+                 var filename=files.headpic.name;
+                 var src=files.headpic.path;
+                 var press = path.join(__dirname,files.headpic.path)
 
-                var pics=path.basename(filename,path.extname(filename))
-                var fileDes='../'+'public'+pics+path.extname(filename)
+                 var pics=path.basename(filename,path.extname(filename))
+                 var fileDes='../'+'public'+pics+path.extname(filename)
 
-                fs.renameSync(press,path.join(path.parse(src).dir,fileDes))
-                console.log('press.............'+press);
+                 fs.renameSync(press,path.join(path.parse(src).dir,fileDes))
+                 console.log('press.............'+press);*/
 
+/*
             let user = {};
             user.userName = fileds.userName;
             user.userPwd = fileds.userPwd;
@@ -74,8 +83,42 @@ module.exports = {
             console.log('**************'+JSON.stringify(user))
             let  jsondata = await DB.updateUsers(user);
             ctx.body = {code: 200, message: 'message', data: jsondata}
-        })
+        },
 
-        ctx.body = '上传成功'
-    }
+        ctx.body = '上传成功'*/
+        var form = new formidable.IncomingForm();
+        form.uploadDir = '../public/myPic';   //设置文件存放路径
+        form.multiples = true;  //设置上传多文件
+        var filename = "";
+        var src = "";
+        var fileDes = "";
+        form.parse(ctx.req, async function (err, fields, files) {
+            // console.log(files)
+            //根据files.filename.name获取上传文件名，执行后续写入数据库的操作
+            filename = files.filename.name;
+            src = path.join(__dirname,'../',files.filename.path);
+            fileDes = path.basename(filename, path.extname(filename)) + moment(new Date()).format("YYYYMMDDHHMMSS") + path.extname(filename);
+            console.log("src : " + src)
+            console.log("dir : " + path.join(path.parse(src).dir))
+            console.log("fileDes" + fileDes)
+            fs.rename(src, path.join(path.parse(src).dir,fileDes));
+            let str = `http://localhost:3000/myPic/${fileDes}`;
+            console.log("str : " + str);
+            console.log("fields : " + fields);
+            console.log("mId:   " + fields.mId);
+            let user = {}
+            user.userName = fields.userName;
+            user.userPhoneNum = fields.userPhoneNum;
+            user.userPwd = fields.userPwd;
+            user.userPic = str;
+            try {
+                await await DB.updateUsers(user);
+                ctx.body={"code":200, "message":"ok", data:[]};
+            } catch (e) {
+                ctx.body={"code":500, "message":"err"+e.message, data:[]};
+            }
+
+            // //根据fileds.mydata获取上传表单元素的数据，执行写入数据库的操作
+        })
+}
 }

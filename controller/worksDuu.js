66a7a88f14f3fe6e2701411c439c625882bdd547
asus <1509592281@qq.com>
2018-11-11 16:57:26 +0800

    gai

--- controller/worksDuu.js
+++ controller/worksDuu.js
@@ -1,5 +1,9 @@
 const DB = require('../model/worksDAO');
-const form = require('formidable');
+const formidable = require('formidable');
+const fs =require('fs');
+const path=require('path');
+const crypto=require('crypto');
+const moment = require('moment');//用于显示时间
 module.exports = {
     getworks: async (ctx) => {
         ctx.set('Access-Control-Allow-Origin', '*');
@@ -20,18 +24,62 @@ module.exports = {
         ctx.body = jsondata;
     },
     addWorks: async (ctx, next) => {
+        console.log('---------------')
         ctx.set('Access-Control-Allow-Origin','*');
-        ctx.set('content-type','application/json');
-        console.log(ctx.request.body)
-        let works = {};
-        works.worksId = ctx.request.body.worksId;
-        works.worksPic = ctx.request.body.worksPic;
-        works.worksauthor = ctx.request.body.worksauthor;
-        works.worksDescribe = ctx.request.body.worksDescribe;
-        works.worksEquipment = ctx.request.body.worksEquipment;
-        works.worksDate = ctx.request.body.worksDate;
-        let jsondata =  await DB.addWorks(works);
-        ctx.body = {code: 200, message: 'message', data: jsondata}
+        ctx.set('content-type','application/json')
+
+        var form = new formidable.IncomingForm();
+        form.uploadDir = '../public/img';   //设置文件存放路径
+        form.multiples = true;  //设置上传多文件
+
+        form.parse(ctx.req,async function (err, fields, files) {
+
+            let heapicName=files.filename.name
+            console.log("头像名称"+heapicName);
+            var now=moment(new Date()).format('YYYYMMDDHHmmss');
+            let comppath=path.join(__dirname,files.filename.path)
+            console.log(comppath+'完整路径');
+            //新名
+            let newname=path.basename(heapicName,path.extname(heapicName))+now+path.extname(heapicName)
+            console.log('LL'+newname);
+            let newp=path.join(__dirname,'../public/img/'+newname)
+            console.log(newp+'新路径完整')
+
+            //重命名
+
+            fs.renameSync(comppath,newp)
+
+            let works = {}
+            works.worksId=fields.worksId;
+            works.worksPic = newname;
+            works.userid= fields.userid;
+            works.worksDescribe = fields.worksDescribe;
+            works.worksEquipment = fields.worksEquipment;
+            works.worksDate = fields.worksDate;
+
+            console.log(fields.worksId)
+            console.log(fields.userid)
+            console.log(fields.worksDescribe)
+            console.log(fields.worksEquipment)
+            console.log(newname)
+            console.log(fields.worksDate)
+
+
+            try{
+                let jsondata=  await DB.addWorks(works);
+                //3.反馈结果
+                ctx.body = {"code": 200, "message": "ok", data:works}
+                // ctx.render('user', {data: jsondata})
+
+            }catch (e) {
+                ctx.body = {"code": 500, "message": err, data: []}
+
+            }
+            if (err) {
+                ctx.body = '上传失败'
+            }
+        })
+        ctx.body = '上传成功'
     },
     updateworks: async (ctx, next) => {
         ctx.set('Access-Control-Allow-Origin','*');
@@ -62,11 +110,6 @@ module.exports = {
         ctx.set('content-type', 'application/json');
         ctx.body = jsondata;
     },
-    getworksD: async (ctx) => {
-        ctx.set('Access-Control-Allow-Origin', '*');
-        let jsondata = await DB.getworksD(ctx.query.id)
-        ctx.set('content-type', 'application/json');
-        ctx.body = jsondata;
-    },
+
 
 }
--- model/worksDAO.js
+++ model/worksDAO.js
@@ -7,12 +7,12 @@ class DB{
     }
     //获取一个作品信息的方法
     getOneWorks(id){
-        return DAO('select * from works where worksId=?',[id])
+        return DAO('SELECT works.worksPic,works.worksEquipment,works.worksDescribe,works.worksDate,users.userName from works,users WHERE works.userid=users.userID and works.worksId=?',[id])
     }
     //添加一个作品信息的方法
     addWorks(works){
         return DAO('insert into  works values(?,?,?,?,?,?)',
-            [works.worksId,works.worksPic,works.worksauthor,works.worksDescribe,works.worksEquipment,works.worksDate])
+            [works.worksId,works.worksPic,works.userid,works.worksDescribe,works.worksEquipment,works.worksDate])
     }
     //删除一个作品信息
     deleteworks (id){
@@ -25,13 +25,11 @@ class DB{
     }
     getnewworks() {
         //获取最新作品信息的方法
-        return DAO('select worksId,worksPic,worksauthor,worksDescribe,worksEquipment,worksDate from works  order by worksDate  desc limit 0,3', [])
+        return DAO('SELECT  works.worksPic,works.worksDescribe,works.worksEquipment,works.worksDate,users.userName  FROM `works`,users WHERE works.userid=users.userID order by worksDate  desc limit 0,3', [])
     }
 getpic(){
     return DAO('select * from pic',[])
 }
-getworksD(id){
-    return DAO('  SELECT worksdet.Dpic FROM worksdet,works WHERE worksdet.worksId=works.worksId and works.worksId=?',[id])
-}
+
 }
 module.exports= new DB();
